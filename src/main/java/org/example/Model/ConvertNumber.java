292460cd6264a6b259bde18d9a166cdca685a4d7
Tobias Felder <tobiasfelder@proton.me>
2023-06-20 21:49:30 +0200

    Added Address Converter

--- src/main/java/org/example/Model/ConvertNumber.java
+++ src/main/java/org/example/Model/ConvertNumber.java
@@ -1,4 +1,81 @@
 package org.example.Model;
 
+import java.util.HashMap;
+
 public class ConvertNumber {
+    public static String hexToBinary(String hex) {
+        // variable to store the converted
+        // Binary Sequence
+        String binary = "";
+
+        // converting the accepted Hexadecimal
+        // string to upper case
+        hex = hex.toUpperCase();
+
+        // initializing the HashMap class
+        HashMap<Character, String> hashMap = new HashMap<Character, String>();
+
+        // storing the key value pairs
+        hashMap.put('0', "0000");
+        hashMap.put('1', "0001");
+        hashMap.put('2', "0010");
+        hashMap.put('3', "0011");
+        hashMap.put('4', "0100");
+        hashMap.put('5', "0101");
+        hashMap.put('6', "0110");
+        hashMap.put('7', "0111");
+        hashMap.put('8', "1000");
+        hashMap.put('9', "1001");
+        hashMap.put('A', "1010");
+        hashMap.put('B', "1011");
+        hashMap.put('C', "1100");
+        hashMap.put('D', "1101");
+        hashMap.put('E', "1110");
+        hashMap.put('F', "1111");
+
+        int i;
+        char ch;
+
+        // loop to iterate through the length
+        // of the Hexadecimal String
+        for (i = 0; i < hex.length(); i++) {
+            // extracting each character
+            ch = hex.charAt(i);
+
+            // checking if the character is
+            // present in the keys
+            if (hashMap.containsKey(ch))
+
+                // adding to the Binary Sequence
+                // the corresponding value of
+                // the key
+                binary += hashMap.get(ch);
+
+                // returning Invalid Hexadecimal
+                // String if the character is
+                // not present in the keys
+            else {
+                binary = "Invalid Hexadecimal String";
+                return binary;
+            }
+        }
+
+        // returning the converted Binary
+        return binary;
+    }
+
+    public static int binaryToDecimal(String binary) {
+        long lbinary = Integer.parseInt(binary);
+        int decimalNumber = 0, i = 0;
+        long remainder;
+
+        while (lbinary != 0) {
+            remainder = lbinary % 10;
+            lbinary /= 10;
+            decimalNumber += remainder * Math.pow(2, i);
+            ++i;
+        }
+
+        return decimalNumber;
+    }
 }
